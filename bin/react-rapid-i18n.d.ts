bb607e9a24dcd51463861f1fc338f7f8a9e37240
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2024-10-22 00:46:11 +0600

    Downgrade version

--- bin/react-rapid-i18n.d.ts
+++
@@ -1,7 +0,0 @@
-import i18n from 'i18next';
-import { useTranslation, Trans } from "react-i18next";
-export declare const _t: (text: any) => string;
-export declare const _tNumber: (text: any) => any;
-export declare const _loadTranslation: (lang: string, translation: any) => void;
-export { i18n, useTranslation, Trans };
-//# sourceMappingURL=react-rapid-i18n.d.ts.map
